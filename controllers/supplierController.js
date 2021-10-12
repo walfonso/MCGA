af4d2dc65c130951599ac6dc79a2c6a4f26e3350
rubensato <rubensato@gmail.com>
2021-10-12 18:28:52 -0300

    added method getAllSuppliers

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -1,1 +1,21 @@
 const Supplier = require('../models/supplier');
+
+
+// GET All Suppliers
+exports.getAllSuppliers = async (req, res) => {
+    try {
+  
+      const suppliers = await Supplier.find();
+      if (suppliers.length === 0)
+        return res.status(400).json('No hay registros de proveedores.');
+      return res.status(200).json(suppliers);
+  
+    } catch (error) {
+  
+      console.error(error);
+      return res.status(500).json({ message: error.message });
+  
+    }
+  };
+  
+  
--- routes/supplier.js
+++ routes/supplier.js
@@ -1,7 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/supplierController');
+const supplierController = require('../controllers/supplierController');
 
 module.exports = () => {
+  router.get('/all', supplierController.getAllSuppliers);
+
   return router;
 };
