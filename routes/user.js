a6d7c041dc37434f2d610c93057e93b5d6fac1b3
walfonso <walfonso@hotmail.com>
2023-11-29 16:49:51 -0300

    add route user

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 module.exports = () => {
-router.post("/login/", userController.login);
+  router.post("/login/", userController.login);
   router.get("/all", userController.getAllUsers);
   router.get("/:userId", userController.getUserById);
   router.delete("/:userId", userController.deleteUser);
